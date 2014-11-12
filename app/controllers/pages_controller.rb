cab18b0c4953a698b6e95d5ab5351bd971b27494
Kierancz <kieran.czerwinski@colorado.edu>
2014-11-11 20:37:12 -0700

    add initial files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+	def home
+	end
+
+	def about
+	end
+
+	def contact
+	end
+
+	def photography
+	end
+
+	def projects
+	end
+
+	def works
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KieranCz::Application.config.secret_key_base = 'cd867182acf37b03a6e4e2202ae352aae6dbabb4f3ef6c80a95acac044e493f7d34b7539a1c65033dca78a0bb93f1aa0f03f532ac1cb5551d6af02ed829b972c'
